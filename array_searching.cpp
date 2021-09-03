117a57e77b5023d6e00cd9907d24e1202fb5da9a
Atharvbhaise17 <85877870+Atharvbhaise17@users.noreply.github.com>
2021-09-03 08:51:52 +0530

    Add files via upload

---
+++ array_searching.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+ 
+   
+   int array[5]={5,14,45,8,89};
+
+for(int i=0;i<5;i++){
+
+  if(array[i]==8){
+        cout<<i;
+  }
+   
+}
+    return 0;
+}
---
+++ simple_array.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+    int array[4]={10,20,30,40};
+
+    cout<<array[0]<<endl;
+    cout<<array[1]<<endl;
+    cout<<array[2]<<endl;
+    cout<<array[3]<<endl;
+
+ 
+
+    return 0;
+}
